c4b982b94992508a1b0a4250648074ff88e4efcd
nikita <jadhavniki2697@gmail.com>
2020-05-23 16:49:44 +0530

    another exercise

---
+++ tut_26.py
@@ -0,0 +1,11 @@
+import random
+rand_num=random.randint(0,100)#gnrt int numb btwn rng
+#print(rand_num)
+rand_num2=random.random()#gnrt numbr btwn 0 to 1 and if u wnt gnrt btwn 0 to 15 mutiply with 15
+print(rand_num2)
+rand_num3=random.random()*15
+print(rand_num3)
+
+list=["nikita","ajay","akshu","rishi"]
+choic1=random.choice(list)
+print(choic1)
